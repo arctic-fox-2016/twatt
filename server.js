cfcc4c0c5619874fc3fd354d2c237af927098cd0
ivangerard <ivan.gerard@hotmail.com>
2016-09-27 13:32:38 +0700

    oauth success

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ let routes = require('./routes/user.js')
 let jwt = require('jsonwebtoken')
 var model = require("./models/index")
 let index = require('./routes/index.js')
+var OAuth = require('oauth');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -46,6 +47,48 @@ app.use('/api', routes)
 
 app.use('/', index)
 
+var access_token = "154606959-CkzjYmJ93H9GxDsTWDaPFfcw2RzBPgW44sbK1VwX"
+var token_secret = "PJItyyTeBgig31k5802KFmNbtoUvtx16WS6SwwtozQ2F8"
+var consumer_key = "zUjbQkhlFDuItROxO7PAxmtoa"
+var consumer_secret = "cdBJmWIzgOHOmKbqpU7TaDLMGSRNmYSsJQrKdJOlHkq7Rn2YFh"
+
+
+//
+// app.get('/sessions/connect', function(req, res) {
+//     consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results) {
+//         if (error) {
+//             res.send("Error getting OAuth request token : " + util.inspect(error), 500);
+//         } else {
+//             req.session.oauthRequestToken = oauthToken;
+//             req.session.oauthRequestTokenSecret = oauthTokenSecret;
+//             res.redirect("https://twitter.com/oauth/authorize?oauth_token=" + req.session.oauthRequestToken);
+//         }
+//     });
+// });
+
+
+app.get("/testoauth", function(req, res, next) {
+    var oauth = new OAuth.OAuth(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/access_token',
+        consumer_key,
+        consumer_secret,
+        '1.0A',
+        null,
+        'HMAC-SHA1'
+    );
+    oauth.get(
+        'https://api.twitter.com/1.1/trends/place.json?id=23424977',
+        access_token, //test user token
+        token_secret, //test user secret
+        function(e, data, response) {
+            if (e) console.error(e);
+            console.log(require('util').inspect(data));
+            res.json(JSON.parse(data.trends.name))
+        })
+});
+
+
 
 
 app.post("/authenticate", function(req, res, next) {
@@ -65,8 +108,7 @@ app.post("/authenticate", function(req, res, next) {
     })
 });
 
-
-app.listen(process.env.PORT, function() {
-    console.log('example app listenings port 3000!');
+app.listen(3000, function() {
+    console.log('example app listenings port 4000!');
 
 })
